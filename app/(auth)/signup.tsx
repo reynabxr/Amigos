4739c03149c5ac5c65ca87e95b1c970dbf60baa9
reyna <171039150+reynabxr@users.noreply.github.com>
2025-05-29 00:47:35 +0800

    screens: made login/sign in page

--- app/(auth)/signup.tsx
+++ app/(auth)/signup.tsx
@@ -3,8 +3,7 @@ import React from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function signup() {
-    const router = useRouter(); // Add this if not already present
-// ...
+    const router = useRouter(); 
 
   return (
     <View style={styles.container}>
