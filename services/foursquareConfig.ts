194b4285d9ca219759c0e6f2125afe14d2385960
estherkyx <157147847+estherkyx@users.noreply.github.com>
2025-06-27 11:02:30 +0800

    more changes

--- services/foursquareConfig.ts
+++
@@ -1,4 +0,0 @@
-export const FSQ_TOKEN =
-  'Bearer 0IVYPELNY4LNMALBKREA520UP1HFILQNEAGKPLHLIRPJKOJ0'
-export const FSQ_API_VERSION = '2025-06-17'
-export const FSQ_BASE = 'https://places-api.foursquare.com/places'
